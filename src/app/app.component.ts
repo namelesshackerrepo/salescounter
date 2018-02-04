ae1a3cd605ee0a55487742aa76993ec085edc444
sethkoch <seth@sethkochs-MacBook-Pro.local>
2018-02-04 10:00:04 -0500

    first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:`
+    <div>Cookies {{ cookiesSold | async}}</div>
+    <button (click)="sellOne()">Sell One</button>
+  `,
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+  cookiesSold =  this.db.list('cookies').valueChanges();
+
+  constructor(
+    private db: AngularFireDatabase
+  ){}
+
+  ngOnInit() {
+    console.log(this.cookiesSold);
+  }
+
+  sellOne() {
+    this.db.object('/cookies/number').query.ref.transaction( x => x + 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule, FirebaseAppConfig } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig: FirebaseAppConfig = {
+  apiKey: 'AIzaSyCixVKShBv5Ye9iSVVcmgj4hdOceNWBGEo',
+  authDomain: 'scoutsales-dc763.firebaseapp.com',
+  databaseURL: 'https://scoutsales-dc763.firebaseio.com',
+  projectId: 'scoutsales-dc763',
+  storageBucket: 'scoutsales-dc763.appspot.com',
+  messagingSenderId: '74807490605'
+};
 
 
 @NgModule({
@@ -10,7 +22,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
